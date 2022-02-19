b2cab15178bcd981b979fe564a897f1f24b4fba6
sahumo <7523460@gmail.com>
2022-02-19 15:23:33 +0200

    1

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
 "use strict";
 
-//Цепочка прототипов может быть длиннее:
+let str ="some";
+let strObj = new String(str);
+console.log(typeof(str));
+console.log(typeof(strObj));
 
-let animal = {
-  eats: true,
-  walk() {
-    alert("Animal walk");
-  }
-};
-
-let rabbit = {
-  jumps: true,
-  __proto__: animal
-};
-
-let longEar = {
-  earLength: 10,
-  __proto__: rabbit
-};
-
-// walk взят из цепочки прототипов
-longEar.walk(); // Animal walk
-alert(longEar.jumps); // true (из rabbit)
-
-////Есть только два ограничения:
-
-//Ссылки не могут идти по кругу. JavaScript выдаст ошибку, если мы попытаемся назначить __proto__ по кругу.
-//Значение __proto__ может быть объектом или null. Другие типы игнорируются.
-//Это вполне очевидно, но всё же: может быть только один [[Prototype]]. Объект не может наследоваться от двух других объектов.
+//[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\20_osnovi_OOP_ptototipno_orientirovannoe_nasledovanie\tempCodeRunnerFile.js"
+//string
+//object
