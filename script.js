2173598849c88024cd5e3b39f4721cc77a7ab72e
sahumo <7523460@gmail.com>
2022-02-19 14:32:59 +0200

    3  пример  с  лерн.джаваскрипт

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 "use strict";
 
+//Цепочка прототипов может быть длиннее:
+
 let animal = {
-    eats: true,
-    walk() {
-      alert("Animal walk");
-    }
-  };
-  
-  let rabbit = {
-    jumps: true,
-    __proto__: animal
-  };
-  
-  // walk взят из прототипа
-  rabbit.walk(); // Animal walk
+  eats: true,
+  walk() {
+    alert("Animal walk");
+  }
+};
 
-  //Если у нас есть метод в animal, он может быть вызван на rabbit:
+let rabbit = {
+  jumps: true,
+  __proto__: animal
+};
 
+let longEar = {
+  earLength: 10,
+  __proto__: rabbit
+};
+
+// walk взят из цепочки прототипов
+longEar.walk(); // Animal walk
+alert(longEar.jumps); // true (из rabbit)
+
+////Есть только два ограничения:
+
+//Ссылки не могут идти по кругу. JavaScript выдаст ошибку, если мы попытаемся назначить __proto__ по кругу.
+//Значение __proto__ может быть объектом или null. Другие типы игнорируются.
+//Это вполне очевидно, но всё же: может быть только один [[Prototype]]. Объект не может наследоваться от двух других объектов.
