7ad83ec71536fb3d1552916acc5853c6146a3c03
sahumo <7523460@gmail.com>
2022-02-19 14:23:19 +0200

    пример  с  лерн.джаваскрипт

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 "use strict";
 
-
+let animal = {
+    eats: true
+  };
+  let rabbit = {
+    jumps: true
+  };
+  
+  rabbit.__proto__ = animal; // (*)
+  
+  // теперь мы можем найти оба свойства в rabbit:
+  alert( rabbit.eats ); // true (**)
+  alert( rabbit.jumps ); // true
 
