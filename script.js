d9d268d64e912372b91d1e5f2fb23eec352f7f41
sahumo <7523460@gmail.com>
2022-02-19 14:26:30 +0200

    2 пример  с  лерн.джаваскрипт

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 "use strict";
 
 let animal = {
-    eats: true
+    eats: true,
+    walk() {
+      alert("Animal walk");
+    }
   };
+  
   let rabbit = {
-    jumps: true
+    jumps: true,
+    __proto__: animal
   };
   
-  rabbit.__proto__ = animal; // (*)
-  
-  // теперь мы можем найти оба свойства в rabbit:
-  alert( rabbit.eats ); // true (**)
-  alert( rabbit.jumps ); // true
+  // walk взят из прототипа
+  rabbit.walk(); // Animal walk
+
+  //Если у нас есть метод в animal, он может быть вызван на rabbit:
 
