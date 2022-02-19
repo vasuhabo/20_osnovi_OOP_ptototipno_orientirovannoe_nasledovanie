c696818519c1c99a60e8137aab24849908ee1557
sahumo <7523460@gmail.com>
2022-02-19 15:31:52 +0200

    2

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 
 let str ="some";
 let strObj = new String(str);
-console.log(typeof(str));
-console.log(typeof(strObj));
 
-//[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\20_osnovi_OOP_ptototipno_orientirovannoe_nasledovanie\tempCodeRunnerFile.js"
-//string
-//object
+console.dir([1,2,3]);
+//  otkrit v  broweser  v  console
