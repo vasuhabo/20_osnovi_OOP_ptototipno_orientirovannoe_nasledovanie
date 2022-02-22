57affb7206a36e51679c5fcc1b7e8fb1d96a4650
sahumo <7523460@gmail.com>
2022-02-22 21:51:23 +0200

    3

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 "use strict";
 
-let str ="some";
-let strObj = new String(str);
 
-console.dir([1,2,3]);
-//  otkrit v  broweser  v  console
+
+const soldier = {
+health:400,
+armor:100
+
+};
+
+const jonh = {
+    health:100
+};
+
+jonh.__proto__ = soldier;
+
+console.log(jonh);
+
+//[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\20_osnovi_OOP_ptototipno_orientirovannoe_nasledovanie\tempCodeRunnerFile.js"
+//{ health: 100 }
